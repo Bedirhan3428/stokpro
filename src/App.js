56a21533a5e15575082caa1d0b255cc0b00c4203
Bedirhan <abimer2350@gmail.com>
2025-12-17 21:58:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 import RequireAuth from "./components/RequireAuth";
 const Home = React.lazy(() => import("./components/Home"));
-const Home = React.lazy(() => import("./components/ProductKey"));
+const ProductKey = React.lazy(() => import("./components/ProductKey"));
 const Dashboard = React.lazy(() => import("./components/Dashboard"));
 const Products = React.lazy(() => import("./components/Products"));
 const Sales = React.lazy(() => import("./components/Sales"));
