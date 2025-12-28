c1383cb36118e9e94cb592d3570cc0b77dc29f9c
Bedirhan <abimer2350@gmail.com>
2025-12-28 14:09:18 +0300

    Update PrivacyPolicy.js

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -56,7 +56,7 @@ export default function PrivacyPolicy() {
           <p className="legal-metin">
             Bu Gizlilik Politikası hakkında sorularınız varsa, lütfen bizimle iletişime geçin:
             <br />
-            <a href="mailto:destek@stokpro.shop" className="legal-link">stokproresmi@gmail.com</a>
+            <a href="mailto:stokproresmi@gmail.com" className="legal-link">destek@stokpro.shop</a>
           </p>
         </div>
       </div>
