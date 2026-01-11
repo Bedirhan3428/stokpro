ecbc4c0caeb61d24d5c88098ee1e0b3358d91c94
Bedirhan <abimer2350@gmail.com>
2026-01-11 13:47:32 +0300

    Update AdminDashboard.js

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "../styles/AdminDashboard.css"; temasının CSS dosyası
+import "../styles/AdminDashboard.css"; 
 import { auth, db } from './firebase'; 
 import { 
   doc, 
