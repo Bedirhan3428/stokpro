4b6d73ddf918207cbfe27444d95d6ae327e983fd
Bedirhan <abimer2350@gmail.com>
2026-01-16 15:11:34 +0300

    Update TermsOfService.js

--- src/TermsOfService.js
+++ src/TermsOfService.js
@@ -2,7 +2,7 @@ import "./styles/Legal.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function TermsOfService() {
+function TermsOfService() {
   return (
     <div className="legal-kapsul">
       <div className="legal-icerik">
@@ -66,3 +66,5 @@ export default function TermsOfService() {
     </div>
   );
 }
+
+export default TermsOfService;
